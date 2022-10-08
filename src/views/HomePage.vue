3b6f0941f22a6e6f700861ebe688bb82806864cc
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-10-08 15:28:54 -0500

    changed bg and button color

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -16,12 +16,12 @@ onMounted(async () => {
 <template>
   <button
     title="Load Characters"
-    class="z-90 fixed bottom-8 right-8 flex h-20 w-20 items-center justify-center rounded-full bg-green-500 text-4xl text-white drop-shadow-lg duration-300 hover:scale-110 hover:bg-slate-500 hover:drop-shadow-2xl"
+    class="z-90 fixed bottom-8 right-8 flex h-20 w-20 items-center justify-center rounded-full bg-red-300 text-4xl text-white drop-shadow-lg duration-300 hover:scale-110 hover:bg-slate-500 hover:drop-shadow-2xl"
     @click="fetchCharacters"
   >
     🚀
   </button>
-  <main class="min-h-screen bg-gradient-to-r from-fuchsia-500 to-red-600">
+  <main class="min-h-screen bg-gradient-to-r from-blue-700 to-green-500">
     <div class="container mx-auto grid grid-cols-8 gap-4 py-8">
       <BaseCard v-for="character in characters" :key="character._id" :character="character" />
     </div>
